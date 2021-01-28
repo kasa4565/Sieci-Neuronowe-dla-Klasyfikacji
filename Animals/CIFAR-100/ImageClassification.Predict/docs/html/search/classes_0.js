6ca88497f31668e2157b6df07c52e694149d8eb2
Sebastian Bobrowski <sebastian.bobrowski@outlook.com>
2021-01-28 21:52:49 +0100

    updates

--- Animals/CIFAR-100/ImageClassification.Predict/docs/html/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['program_18',['Program',['../class_image_classification_1_1_predict_1_1_program.html',1,'ImageClassification::Predict']]]
-];
--- Animals/CIFAR-100/ImageClassification.Predict/docs/html/search/namespaces_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['imageclassification_19',['ImageClassification',['../namespace_image_classification.html',1,'']]],
-  ['predict_20',['Predict',['../namespace_image_classification_1_1_predict.html',1,'ImageClassification']]]
-];
