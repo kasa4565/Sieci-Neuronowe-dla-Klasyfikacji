e0194e445a1e851901bed432b774ee5cc4d279f6
Sebastian Bobrowski <sebastian.bobrowski@outlook.com>
2021-01-29 20:08:01 +0100

    updates

---
+++ Rice Leaf Diseases/ImageClassification.Predict/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ImageClassification.DataModels;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace ImageClassification.Predict
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Start the program.
+        /// </summary>
+        private static void Main()
+        {
+            string assetsPath = GetAssetsPath();
+            string imagesFolderPathForPredictions = GetImagesPath(assetsPath);
+            string imageClassifierModelZipFilePath = GetClassifierModelPath(assetsPath);
+
+            try
+            {
+                var mlContext = new MLContext(seed: 1);
+                Console.WriteLine($"Loading model from: {imageClassifierModelZipFilePath}");
+                ITransformer loadedModel = GetLoadedModel(imageClassifierModelZipFilePath, mlContext);
+                var predictionEngine = GetPredictionEngine(mlContext, loadedModel);
+                var imagesToPredict = GetImagesToPredict(imagesFolderPathForPredictions);
+                var imageToPredict = imagesToPredict.First();
+                var prediction = GetFirstPrediction(predictionEngine, imageToPredict);
+                DoSecondPrediction(predictionEngine, imageToPredict);
+                DoubleCheckUsingIndex(predictionEngine, prediction);
+                PrintInformationAboutPrediction(imageToPredict, prediction);
+                PredictAllImages(predictionEngine, imagesToPredict);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.WriteLine("Press any key to end the app..");
+            Console.ReadKey();
+        }
+
+
+        /// <summary>
+        /// Predict all images in the folder.
+        /// </summary>
+        /// <param name="predictionEngine"></param>
+        /// <param name="imagesToPredict"></param>
+        private static void PredictAllImages(PredictionEngine<InMemoryImageData, ImagePrediction> predictionEngine, IEnumerable<InMemoryImageData> imagesToPredict)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Predicting several images...");
+
+            foreach (var currentImageToPredict in imagesToPredict)
+            {
+                var currentPrediction = predictionEngine.Predict(currentImageToPredict);
+                PrintInformationAboutPrediction(currentImageToPredict, currentPrediction);
+            }
+        }
+
+        private static void PrintInformationAboutPrediction(InMemoryImageData imageToPredict, ImagePrediction prediction)
+        {
+            Console.WriteLine($"Image Filename : [{imageToPredict.ImageFileName}], " +
+                                              $"Predicted Label : [{prediction.PredictedLabel}], " +
+                                              $"Probability : [{prediction.Score.Max()}] "
+                                              );
+        }
+
+        /// <summary>
+        /// Double-check using the index.
+        /// </summary>
+        /// <param name="predictionEngine"></param>
+        /// <param name="prediction"></param>
+        private static void DoubleCheckUsingIndex(PredictionEngine<InMemoryImageData, ImagePrediction> predictionEngine, ImagePrediction prediction)
+        {
+            var maxIndex = prediction.Score.ToList().IndexOf(prediction.Score.Max());
+            VBuffer<ReadOnlyMemory<char>> keys = default;
+            predictionEngine.OutputSchema[3].GetKeyValues(ref keys);
+            var keysArray = keys.DenseValues().ToArray();
+            var predictedLabelString = keysArray[maxIndex];
+        }
+
+        /// <summary>
+        /// Do second prediction for count the taken time.
+        /// </summary>
+        /// <param name="predictionEngine"></param>
+        /// <param name="imageToPredict"></param>
+        private static void DoSecondPrediction(PredictionEngine<InMemoryImageData, ImagePrediction> predictionEngine, InMemoryImageData imageToPredict)
+        {
+            var watch2 = System.Diagnostics.Stopwatch.StartNew();
+
+            predictionEngine.Predict(imageToPredict);
+
+            watch2.Stop();
+            var elapsedMs2 = watch2.ElapsedMilliseconds;
+            Console.WriteLine("Second Prediction took: " + elapsedMs2 + "mlSecs");
+        }
+
+        /// <summary>
+        /// Get the prediction and count the taken time.
+        /// </summary>
+        /// <param name="predictionEngine"></param>
+        /// <param name="imageToPredict"></param>
+        /// <returns>Prediction</returns>
+        private static ImagePrediction GetFirstPrediction(PredictionEngine<InMemoryImageData, ImagePrediction> predictionEngine, InMemoryImageData imageToPredict)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            var prediction = predictionEngine.Predict(imageToPredict);
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("First Prediction took: " + elapsedMs + "mlSecs");
+            return prediction;
+        }
+
+        private static IEnumerable<InMemoryImageData> GetImagesToPredict(string imagesFolderPathForPredictions) => 
+            FileUtils.LoadInMemoryImagesFromDirectory(imagesFolderPathForPredictions, false);
+
+        /// <summary>
+        /// Create prediction engine to try a single prediction (input = ImageData, output = ImagePrediction).
+        /// </summary>
+        /// <param name="mlContext"></param>
+        /// <param name="loadedModel"></param>
+        /// <returns>Prediction Engine</returns>
+        private static PredictionEngine<InMemoryImageData, ImagePrediction> GetPredictionEngine(MLContext mlContext, ITransformer loadedModel) => 
+            mlContext.Model.CreatePredictionEngine<InMemoryImageData, ImagePrediction>(loadedModel);
+
+        /// <summary>
+        /// Load the model.
+        /// </summary>
+        /// <param name="imageClassifierModelZipFilePath"></param>
+        /// <param name="mlContext"></param>
+        /// <returns>Loaded model</returns>
+        private static ITransformer GetLoadedModel(string imageClassifierModelZipFilePath, MLContext mlContext) => 
+            mlContext.Model.Load(imageClassifierModelZipFilePath, out var modelInputSchema);
+
+        public static string GetAbsolutePath(string relativePath) => 
+            FileUtils.GetAbsolutePath(typeof(Program).Assembly, relativePath);
+
+        private static string GetClassifierModelPath(string assetsPath) => Path.Combine(assetsPath, "inputs", "MLNETModel", "imageClassifier.zip");
+
+        private static string GetImagesPath(string assetsPath) => Path.Combine(assetsPath, "inputs", "images-for-predictions");
+
+        private static string GetAssetsPath()
+        {
+            const string assetsRelativePath = @"../../../assets";
+            var assetsPath = GetAbsolutePath(assetsRelativePath);
+            return assetsPath;
+        }
+    }
+}
---
+++ Rice Leaf Diseases/ImageClassification.Train/Program.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Common;
+using ImageClassification.DataModels;
+using Microsoft.ML;
+using Microsoft.ML.Transforms;
+using static Microsoft.ML.Transforms.ValueToKeyMappingEstimator;
+
+namespace ImageClassification.Train
+{
+    internal class Program
+    {
+        private static MLContext mlContext;
+
+        /// <summary>
+        /// Start the program.
+        /// </summary>
+        static void Main()
+        {
+            string outputMlNetModelFilePath = GetOutputModelFilePath();
+            string predictMlNetModelFilePath = GetPredictModelFilePath();
+            string fullImagesetFolderPath = GetFullImagesetFolderPath();
+            string imagesFolderPathForPredictions = GetImagesForPredictionFolderPath();
+
+            mlContext = new MLContext(seed: 1);
+            SpecifyContextFilter();
+
+            IDataView shuffledFullImageFilePathsDataset = GetShuffledFullImageFilePathsDataset(fullImagesetFolderPath, mlContext);
+            IDataView shuffledFullImagesDataset = GetShuffledFullImagesDataset(fullImagesetFolderPath, mlContext, shuffledFullImageFilePathsDataset);
+
+            var trainTestData = mlContext.Data.TrainTestSplit(shuffledFullImagesDataset, testFraction: 0.2);
+            IDataView trainDataView = trainTestData.TrainSet;
+            IDataView testDataView = trainTestData.TestSet;
+
+            var pipeline = GetTrainingPipeline(mlContext, testDataView);
+
+            ITransformer trainedModel = GetTrainedModel(trainDataView, pipeline);
+            EvaluateModel(mlContext, testDataView, trainedModel);
+
+            SaveModel(outputMlNetModelFilePath, mlContext, trainDataView, trainedModel);
+            CopyModelToPredict(outputMlNetModelFilePath, predictMlNetModelFilePath);
+
+            //TrySinglePrediction(imagesFolderPathForPredictions, mlContext, trainedModel);
+
+            Console.WriteLine("Press any key to finish");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Specify MLContext Filter to only show feedback log/traces about ImageClassification
+        /// </summary>
+        /// <param name="mlContext"></param>
+        private static void SpecifyContextFilter()
+        {
+            mlContext.Log += FilterMLContextLog;
+        }
+
+        /// <summary>
+        /// Copy model to predict.
+        /// </summary>
+        /// <param name="outputMlNetModelFilePath"></param>
+        /// <param name="predictMlNetModelFilePath"></param>
+        private static void CopyModelToPredict(string outputMlNetModelFilePath, string predictMlNetModelFilePath)
+        {
+            File.Copy(outputMlNetModelFilePath, predictMlNetModelFilePath, true);
+            Console.WriteLine($"Model copied to: {predictMlNetModelFilePath}");
+        }
+
+        /// <summary>
+        /// Save the model to assets/outputs (You get ML.NET .zip model file and TensorFlow .pb model file).
+        /// </summary>
+        /// <param name="outputMlNetModelFilePath"></param>
+        /// <param name="mlContext"></param>
+        /// <param name="trainDataView"></param>
+        /// <param name="trainedModel"></param>
+        private static void SaveModel(string outputMlNetModelFilePath, MLContext mlContext, IDataView trainDataView, ITransformer trainedModel)
+        {
+            mlContext.Model.Save(trainedModel, trainDataView.Schema, outputMlNetModelFilePath);
+            Console.WriteLine($"Model saved to: {outputMlNetModelFilePath}");
+        }
+
+        /// <summary>
+        /// Train/create the ML model.
+        /// </summary>
+        /// <param name="trainDataView"></param>
+        /// <param name="pipeline"></param>
+        /// <returns>Trained Model</returns>
+        private static ITransformer GetTrainedModel(IDataView trainDataView, Microsoft.ML.Data.EstimatorChain<KeyToValueMappingTransformer> pipeline)
+        {
+            Console.WriteLine("*** Training the image classification model with DNN Transfer Learning on top of the selected pre-trained model/architecture ***");
+            var watch = Stopwatch.StartNew();
+            ITransformer trainedModel = pipeline.Fit(trainDataView);
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            Console.WriteLine($"Training with transfer learning took: {elapsedMs / 1000} seconds");
+            return trainedModel;
+        }
+
+        /// <summary>
+        /// Define the model's training pipeline using DNN default values.
+        /// </summary>
+        /// <param name="mlContext"></param>
+        /// <param name="testDataView"></param>
+        /// <returns>Training Pipeline</returns>
+        private static Microsoft.ML.Data.EstimatorChain<KeyToValueMappingTransformer> GetTrainingPipeline(MLContext mlContext, IDataView testDataView) =>
+            mlContext.MulticlassClassification.Trainers
+                    .ImageClassification(featureColumnName: "Image",
+                                         labelColumnName: "LabelAsKey",
+                                         validationSet: testDataView)
+                .Append(mlContext.Transforms.Conversion.MapKeyToValue(outputColumnName: "PredictedLabel",
+                                                                      inputColumnName: "PredictedLabel"));
+
+        /// <summary>
+        /// Load Images with in-memory type within the IDataView and Transform Labels to Keys (Categorical).
+        /// </summary>
+        /// <param name="fullImagesetFolderPath"></param>
+        /// <param name="mlContext"></param>
+        /// <param name="shuffledFullImageFilePathsDataset"></param>
+        /// <returns>Shuffled Full Images Dataset</returns>
+        private static IDataView GetShuffledFullImagesDataset(string fullImagesetFolderPath, MLContext mlContext, IDataView shuffledFullImageFilePathsDataset) =>
+            mlContext.Transforms.Conversion.
+                                MapValueToKey(outputColumnName: "LabelAsKey", inputColumnName: "Label", keyOrdinality: KeyOrdinality.ByValue)
+                            .Append(mlContext.Transforms.LoadRawImageBytes(
+                                                            outputColumnName: "Image",
+                                                            imageFolder: fullImagesetFolderPath,
+                                                            inputColumnName: "ImagePath"))
+                            .Fit(shuffledFullImageFilePathsDataset)
+                            .Transform(shuffledFullImageFilePathsDataset);
+
+        /// <summary>
+        /// Load the initial full image-set into an IDataView and shuffle so it'll be better balanced.
+        /// </summary>
+        /// <param name="fullImagesetFolderPath"></param>
+        /// <param name="mlContext"></param>
+        /// <returns>Shuffled Full Image File Paths Dataset</returns>
+        private static IDataView GetShuffledFullImageFilePathsDataset(string fullImagesetFolderPath, MLContext mlContext)
+        {
+            IEnumerable<ImageData> images = LoadImagesFromDirectory(folder: fullImagesetFolderPath, useFolderNameAsLabel: true);
+            IDataView fullImagesDataset = mlContext.Data.LoadFromEnumerable(images);
+            IDataView shuffledFullImageFilePathsDataset = mlContext.Data.ShuffleRows(fullImagesDataset);
+            return shuffledFullImageFilePathsDataset;
+        }
+
+        private static string GetImagesForPredictionFolderPath()
+        {
+            string assetsRelativePath = @"../../../assets";
+            string assetsPath = GetAbsolutePath(assetsRelativePath);
+            return Path.Combine(assetsPath, "inputs", "test-images");
+        }
+
+        private static string GetPredictModelFilePath()
+        {
+            string solutionRelativePath = @"../../../../";
+            string solutionPath = GetAbsolutePath(solutionRelativePath);
+            return Path.Combine(solutionPath, "ImageClassification.Predict", "assets", "inputs", "MLNETModel", "imageClassifier.zip");
+        }
+
+        private static string GetOutputModelFilePath()
+        {
+            string assetsRelativePath = @"../../../assets";
+            string assetsPath = GetAbsolutePath(assetsRelativePath);
+            return Path.Combine(assetsPath, "outputs", "imageClassifier.zip");
+        }
+
+        private static string GetFullImagesetFolderPath()
+        {
+            string assetsRelativePath = @"../../../assets";
+            string assetsPath = GetAbsolutePath(assetsRelativePath);
+            string imagesDownloadFolderPath = Path.Combine(assetsPath, "inputs", "images");
+            string finalImagesFolderName = "rice_leaf_diseases";
+            string fullImagesetFolderPath = Path.Combine(imagesDownloadFolderPath, finalImagesFolderName);
+
+            return fullImagesetFolderPath;
+        }
+
+        /// <summary>
+        /// Get the quality metrics (accuracy, etc.).
+        /// </summary>
+        /// <param name="mlContext"></param>
+        /// <param name="testDataset"></param>
+        /// <param name="trainedModel"></param>
+        private static void EvaluateModel(MLContext mlContext, IDataView testDataset, ITransformer trainedModel)
+        {
+            Console.WriteLine("Making predictions in bulk for evaluating model's quality...");
+
+            var watch = Stopwatch.StartNew();
+
+            var predictionsDataView = trainedModel.Transform(testDataset);
+
+            var metrics = mlContext.MulticlassClassification.Evaluate(predictionsDataView, labelColumnName:"LabelAsKey", predictedLabelColumnName: "PredictedLabel");
+            ConsoleHelper.PrintMultiClassClassificationMetrics("TensorFlow DNN Transfer Learning", metrics);
+
+            watch.Stop();
+            var elapsed2Ms = watch.ElapsedMilliseconds;
+
+            Console.WriteLine($"Predicting and Evaluation took: {elapsed2Ms / 1000} seconds");
+        }
+
+        /// <summary>
+        /// Try a single prediction simulating an end-user app.
+        /// </summary>
+        /// <param name="imagesFolderPathForPredictions"></param>
+        /// <param name="mlContext"></param>
+        /// <param name="trainedModel"></param>
+        private static void TrySinglePrediction(string imagesFolderPathForPredictions, MLContext mlContext, ITransformer trainedModel)
+        {
+            var predictionEngine = mlContext.Model
+                .CreatePredictionEngine<InMemoryImageData, ImagePrediction>(trainedModel);
+
+            var testImages = FileUtils.LoadInMemoryImagesFromDirectory(
+                imagesFolderPathForPredictions, false);
+
+            var imageToPredict = testImages.First();
+
+            var prediction = predictionEngine.Predict(imageToPredict);
+
+            Console.WriteLine(
+                $"Image Filename : [{imageToPredict.ImageFileName}], " +
+                $"Scores : [{string.Join(",", prediction.Score)}], " +
+                $"Predicted Label : {prediction.PredictedLabel}");
+        }
+
+
+        public static IEnumerable<ImageData> LoadImagesFromDirectory(
+            string folder,
+            bool useFolderNameAsLabel = true)
+            => FileUtils.LoadImagesFromDirectory(folder, useFolderNameAsLabel)
+                .Select(x => new ImageData(x.imagePath, x.label));
+
+        public static string GetAbsolutePath(string relativePath)
+            => FileUtils.GetAbsolutePath(typeof(Program).Assembly, relativePath);
+
+        public static void ConsoleWriteImagePrediction(string ImagePath, string Label, string PredictedLabel, float Probability)
+        {
+            var defaultForeground = Console.ForegroundColor;
+            var labelColor = ConsoleColor.Magenta;
+            var probColor = ConsoleColor.Blue;
+
+            Console.Write("Image File: ");
+            Console.ForegroundColor = labelColor;
+            Console.Write($"{Path.GetFileName(ImagePath)}");
+            Console.ForegroundColor = defaultForeground;
+            Console.Write(" original labeled as ");
+            Console.ForegroundColor = labelColor;
+            Console.Write(Label);
+            Console.ForegroundColor = defaultForeground;
+            Console.Write(" predicted as ");
+            Console.ForegroundColor = labelColor;
+            Console.Write(PredictedLabel);
+            Console.ForegroundColor = defaultForeground;
+            Console.Write(" with score ");
+            Console.ForegroundColor = probColor;
+            Console.Write(Probability);
+            Console.ForegroundColor = defaultForeground;
+            Console.WriteLine("");
+        }
+
+        private static void FilterMLContextLog(object sender, LoggingEventArgs e)
+        {
+            if (e.Message.StartsWith("[Source=ImageClassificationTrainer;"))
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
+
